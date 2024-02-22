26e0d6124b35c7b596e5d5556f6927db672e702c
parangodev <pabloarangobte@gmail.com>
2024-02-21 20:18:31 -0600

    Kinde Login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,14 +16,37 @@ export const metadata: Metadata = {
   description: "Información importante de vehículos",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { isAuthenticated } = getKindeServerSession();
+  const isAuthed = await isAuthenticated();
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="flex justify-between items-center py-8 px-24">
+          <h1 className="font-bold text-2xl">Prometeo</h1>
+
+          {isAuthed ? (
+            <Button variant={"secondary"}>
+              <LogoutLink> Logout </LogoutLink>
+            </Button>
+          ) : (
+            <div className="flex gap-2">
+              <LoginLink>
+                <Button variant={"secondary"}>Login</Button>
+              </LoginLink>
+              <RegisterLink>
+                <Button>Register</Button>
+              </RegisterLink>
+            </div>
+          )}
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="px-24">
+      <Card className="rounded-2xl bg-slate-950 shadow-2xl text-white text-center h-[760px]">
+        <div className="mx-auto max-w-[683px] mt-[215px]">
+          <h1 className="text-6xl font-bold leading-tight">
+            Tools to support your online business
+          </h1>
+          <p className="text-3xl mt-2">Start your free trial today</p>
+          <Button className="mt-12" variant={"secondary"} size={"lg"}>
+            Sign up
+          </Button>
+        </div>
+      </Card>
+    </main>
   );
 }
